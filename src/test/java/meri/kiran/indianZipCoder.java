38358065ad74490fbea93899ccd1fc933f52f1c2
TenForBenJamin <sibinsurendran49@gmail.com>
2022-05-17 16:57:00 +0200

    berlin zipcodes

--- src/test/java/meri/kiran/indianZipCoder.java
+++ src/test/java/meri/kiran/indianZipCoder.java
@@ -28,8 +28,8 @@ public class indianZipCoder extends parama  {
 		
 		RestAssured.baseURI =baseUrlopenWeather;
 		String varan=marwahi;
-		int startZip=495100;
-		int endZip=startZip+1000;
+		int startZip=603000;
+		int endZip=startZip+100;
 		for(int i=startZip;i<endZip;i++)
 		{
 			
@@ -53,15 +53,95 @@ public class indianZipCoder extends parama  {
 		}
 //		
 	}
+	
+	
+	@Parameters({"siu"})
 	@Test
-	public void zipSeries_StateLevel()
+	public void zipSeries_StateLevel(int siu)
 	{
 		System.out.println("  zipSeries_StateLevel ");
 		
 		RestAssured.baseURI =baseUrlopenWeather;
 		String varan=marwahi;
-		int startZip=673500;
-		int endZip=startZip+100;
+		int startZip=673001;
+		// kadalundi 673302
+		int endZip=startZip+siu;
+		for(int i=startZip;i<endZip;i++)
+		{
+			
+			try {
+				String zipLoop=i+",IN";
+				// Get the RequestSpecification of the request that you want to sent
+				// to the server. The server is specified by the BaseURI that we have
+				// specified in the above step.
+				RequestSpecification httpRequest = RestAssured.given();
+
+				// Make a GET request call directly by using RequestSpecification.get() method.
+				// Make sure you specify the resource name.
+				Response response = httpRequest
+						.queryParam("zip", zipLoop).
+						queryParam("appid", apiKey).
+						queryParam("lang", OpCo).queryParam("units", "metric").
+						when().get("data/2.5/weather");
+
+				// Response.asString method will directly return the content of the body
+				// as String.
+				//system.out.println("Response Body is =>  " + response.asString());
+				String getReqRes =response.asString();
+				reUsableMethods sd = new reUsableMethods();
+				//sd.simblePrint(getReqRes,zipLoop);
+				sd.complexPrint(getReqRes,zipLoop,404);
+			}
+			catch(Exception e) 
+			{
+				System.out.println(" no postal code found ");
+			}
+		}
+//		
+	}
+	@Parameters({"siu"})
+	@Test
+	public void zipSeries_Berlin(int siu)
+	{
+		System.out.println("  Zip - India ");
+		
+		RestAssured.baseURI =baseUrlopenWeather;
+		String varan=marwahi;
+		int startZip=10700;
+		int endZip=startZip+siu;
+		for(int i=startZip;i<endZip;i++)
+		{
+			
+			try {
+				String zipLoop=i+",DE";
+				String getReqRes =
+						given().
+						queryParam("zip", zipLoop).
+						queryParam("appid", apiKey).
+						queryParam("lang", OpCo).queryParam("units", "metric").
+						when().get("data/2.5/weather").
+						then().extract().response().asString();
+						JsonPath js = new JsonPath(getReqRes);
+						reUsableMethods sd = new reUsableMethods();
+						sd.simblePrint(getReqRes,zipLoop);
+			}
+			catch(Exception e) 
+			{
+				System.out.println(" no postal code found ");
+			}
+		}
+//		
+	}
+	@Parameters({"siu"})
+	@Test
+	public void zipSeries_Chennai(int siu)
+	{
+		System.out.println("  zipSeries_Chennai " +siu);
+		//int counter =Integer.getInteger(siu);
+		RestAssured.baseURI =baseUrlopenWeather;
+		String varan=marwahi;
+		int startZip=603000;
+		int endZip=startZip+siu;
 		for(int i=startZip;i<endZip;i++)
 		{
 			
