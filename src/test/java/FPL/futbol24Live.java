ec3c9f075220f941266b0599dba7ca89e92b60d6
TenForBenJamin <73674410+TenForBenJamin@users.noreply.github.com>
2022-11-04 22:38:22 +0100

    setProperty historic change

--- src/test/java/FPL/futbol24Live.java
+++ src/test/java/FPL/futbol24Live.java
@@ -230,9 +230,37 @@ public class futbol24Live extends parama{
             String  deutschName = js.getString("[0].translations.deu.official");
             String  capital = js.getString("[0].capital[0]");
             String  fifaCode = js.getString("[0].fifa");
-
+            int count=js.getInt("timezones.size()");
 
             return countryName;
+        }else
+            return "400 error ";
+
+    }
+    public String restCOuntriesTimeZone(String coutryCode){
+
+        RestAssured.baseURI ="https://restcountries.com/";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.request(Method.GET,"/v3.1/alpha/" +coutryCode);
+        if(response.statusCode()==200){
+            String getReqRes =
+                    given().
+                            when().get("/v3.1/alpha/" +coutryCode).
+                            then().assertThat().statusCode(200).extract().response().asString();
+            JsonPath js = new JsonPath(getReqRes);
+            reUsableMethods sd = new reUsableMethods();
+            String  countryName = js.getString("[0].name.common");
+            String  deutschName = js.getString("[0].translations.deu.official");
+            String  capital = js.getString("[0].capital[0]");
+            String  fifaCode = js.getString("[0].fifa");
+            int count=js.getInt("timezones.size()");
+            String timeZones="Total timeZones for " + countryName +" = " ;
+            for(int i=0;i<count;i++){
+                String currentTMZ=js.getString("timezones[ " +i +"]");
+                timeZones=timeZones +" " +(i+1) +" " +currentTMZ;
+            }
+
+            return timeZones;
         }else
             return "400 error ";
 
@@ -240,7 +268,8 @@ public class futbol24Live extends parama{
 @Test
     public void trueCaller()
     {
-        String fullCountryName =restCOuntries("za");
+       // String fullCountryName =restCOuntries("dz");
+        String fullCountryName =restCOuntriesTimeZone("gb");
         System.out.println(fullCountryName);
     }
 }
