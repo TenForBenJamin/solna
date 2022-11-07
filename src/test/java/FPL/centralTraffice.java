562a560146ad0aa12e775c7708fcc71f1db16f11
TenForBenJamin <73674410+TenForBenJamin@users.noreply.github.com>
2022-11-08 00:20:27 +0100

    interfaceNabstractClass

---
+++ src/test/java/FPL/centralTraffice.java
@@ -0,0 +1,10 @@
+package FPL;
+
+public interface centralTraffice {
+
+    public void greenGo();
+    public void redStop();
+    public void yellowWait();
+
+
+}
--- src/test/java/FPL/futbol24Live.java
+++ src/test/java/FPL/futbol24Live.java
@@ -90,7 +90,7 @@ public class futbol24Live extends parama{
         List<WebElement> xpathFinder   = driver.findElements(By.xpath("//td[@class='home']"));
         int count= xpathFinder.size();
         System.out.println("total matches = " +count);
-        for(int i=1;i<=10;i++)
+        for(int i=1;i<=count;i++)
         {
             String homeTeam=xpathFinder.get(i).getText();
             homeTeam=homeTeam.trim();
@@ -272,4 +272,29 @@ public class futbol24Live extends parama{
         String fullCountryName =restCOuntriesTimeZone("gb");
         System.out.println(fullCountryName);
     }
+
+    public static int solution(int N) {
+        // write your code in Java SE 8
+        String value = Integer.toBinaryString(N);
+        int counter = 0;
+        System.out.println("binary value is " +value);
+        List<Integer> counters = new ArrayList<>();
+        for (int i = 0; i < value.length(); i++) {
+            char current = value.charAt(i);
+            if (current == '0') {
+                counter += 1;
+            } else {
+                counters.add(counter);
+                counter = 0;
+            }
+        }
+
+        return counters.size();
+    }
+@Test
+    public static void raqani(){
+
+        System.out.println(solution(33));
+    }
+
 }
