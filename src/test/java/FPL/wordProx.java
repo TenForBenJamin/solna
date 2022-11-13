51699d7da64e1f2b07f4f660932490dfc306f3a0
TenForBenJamin <73674410+TenForBenJamin@users.noreply.github.com>
2022-11-13 16:07:35 +0100

    v2 wordProcessor Update

--- src/test/java/FPL/wordProx.java
+++ src/test/java/FPL/wordProx.java
@@ -8,11 +8,17 @@ public class wordProx {
     public static String wordProcesserV2(String homeTeam)
     {
         HashSet<String> hs = new HashSet<String>();
+        // next additions - C. IF fB SE
         hs.add("United");
         hs.add("City");
+        hs.add("IL");
+        hs.add("B");
         hs.add("FC");
         hs.add("FK");
         hs.add("U20");
+        hs.add("II");
+        hs.add("32");
+        hs.add("MTE");
         hs.add("U19");
         hs.add("U16");
         hs.add("U17");
