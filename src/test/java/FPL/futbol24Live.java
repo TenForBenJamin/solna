7db15c679e7c274efa1b7353698f75e45e54710d
TenForBenJamin <73674410+TenForBenJamin@users.noreply.github.com>
2023-06-07 14:27:42 +0200

    1st  fix 7/6  0 amends liveGames fix

--- src/test/java/FPL/futbol24Live.java
+++ src/test/java/FPL/futbol24Live.java
@@ -131,13 +131,13 @@ public class futbol24Live extends parama{
             alCounter++;
         }
         System.out.println("Total Failure/Success is " + failureCount + " / " + statCount +" = " +(double)failureCount/(double)statCount +" total Nations involved  " +nat.size());
-        System.out.println("methodUtilzationCount --- " +methodUtilzationCount );
+        System.out.println("methodUtilzationCount --- " +methodUtilzationCount );/*
         coldestCountry=getCountryName(coldestCountryTwoLetter);
         hottestCountry=getCountryName(hottestCountryTwoLetter);
         System.out.println("coldest place amongst the places is "+coldestPlace +" , "  + coldestCountry +" and is - away  "  +f24Distance(coldestPlace));
         System.out.println("hottest place amongst the places is "+hotPlace +" , "  + hottestCountry +" and is - away  "  +f24Distance(hotPlace));
         System.out.println("Bordering coldest country --- " +restCountriesBoundary(coldestCountryTwoLetter) );
-        System.out.println("Bordering hottest country --- " +restCountriesBoundary(hottestCountryTwoLetter) );
+        System.out.println("Bordering hottest country --- " +restCountriesBoundary(hottestCountryTwoLetter) );*/
     }
 
 
